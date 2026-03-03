4a07aa86938d5f2141e20beeb9fb100ec254f090
Wilson Rogerio <rajwii44@gmail.com>
2026-03-03 14:17:25 -0300

    criado a estutura da pagina inicial e side-bar

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Sidebar } from "./core/components/sidebar/sidebar";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Sidebar],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('meu-portfolio');
+  protected readonly title = signal('Wilson Oliveira');
 }
--- src/app/core/components/sidebar/sidebar.ts
+++ src/app/core/components/sidebar/sidebar.ts
@@ -1,8 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  imports: [RouterLink, CommonModule],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.scss',
 })
