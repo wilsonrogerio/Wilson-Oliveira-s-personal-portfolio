c897b5263ec22948192e7f457c479dd83147b51e
Wilson Rogerio <rajwii44@gmail.com>
2026-03-03 14:19:41 -0300

    criado pagina de projetos

--- src/app/features/projects/projects.ts
+++ src/app/features/projects/projects.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, computed, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-projects',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './projects.html',
   styleUrl: './projects.scss',
 })
 export class Projects {
+  searchQuery = signal('');
 
+  projects = signal([
+    { id: 1, title: 'E-commerce API', desc: 'Node.js & MongoDB' },
+    { id: 2, title: 'Dashboard Pro', desc: 'Angular & NgRx' },
+    { id: 3, title: 'App Mobile', desc: 'Ionic Framework' },
+  ]);
+
+  filteredProjects = computed(() => {
+    return this.projects().filter(p =>
+      p.title.toLowerCase().includes(this.searchQuery().toLowerCase())
+    );
+  });
 }
