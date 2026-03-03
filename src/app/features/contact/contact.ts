189b70d534ec30b1d5daccad66341b58c2f89391
Wilson Rogerio <rajwii44@gmail.com>
2026-03-03 14:39:13 -0300

    criado pagina de conatto adicionado scss

--- src/app/features/contact/contact.ts
+++ src/app/features/contact/contact.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { Sidebar } from '../../core/components/sidebar/sidebar';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [Sidebar],
   templateUrl: './contact.html',
   styleUrl: './contact.scss',
 })
