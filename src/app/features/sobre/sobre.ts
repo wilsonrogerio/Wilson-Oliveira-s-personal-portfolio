7c43083b942d1d94aec5a6f93660421b42183f2b
Wilson Rogerio <rajwii44@gmail.com>
2026-03-03 14:31:19 -0300

    criado pagina de conatto adicionado scss

--- src/app/features/sobre/sobre.ts
+++ src/app/features/sobre/sobre.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { Sidebar } from '../../core/components/sidebar/sidebar';
 
 @Component({
   selector: 'app-sobre',
-  imports: [],
+  imports: [Sidebar],
   templateUrl: './sobre.html',
   styleUrl: './sobre.scss',
 })
