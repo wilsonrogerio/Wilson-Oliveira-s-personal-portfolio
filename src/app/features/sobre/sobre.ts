97258244ad32e13b66251b438a53ba1c97a264e4
Wilson Rogerio <rajwii44@gmail.com>
2026-03-03 14:13:27 -0300

    esquema de pastas estruturadas criado

---
+++ src/app/features/sobre/sobre.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sobre',
+  imports: [],
+  templateUrl: './sobre.html',
+  styleUrl: './sobre.scss',
+})
+export class Sobre {
+
+}
