3ff11aa3d8e1f62f58be46741e33f44b58162415
Wilson Rogerio <rajwii44@gmail.com>
2026-03-03 14:39:41 -0300

    corrigido duplicacao do componente de sideBar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./features/home/home').then(c => c.Home)
+  },
+  {
+    path: 'projetos',
+    loadComponent: () => import('./features/projects/projects').then(c => c.Projects)
+  },
+  {
+    path: 'sobre',
+    loadComponent: () => import('./features/sobre/sobre').then(c => c.Sobre)
+  },
+  {
+    path: 'contato',
+    loadComponent: () => import('./features/contact/contact').then(c => c.Contact)
+  },
+  { path: '**', redirectTo: '' } // Fallback para home
+];
