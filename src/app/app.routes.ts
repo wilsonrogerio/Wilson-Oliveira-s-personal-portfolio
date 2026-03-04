50efc62731ba1691b90c87135284d3d8ab249958
Wilson Rogerio <rajwii44@gmail.com>
2026-03-04 18:23:41 -0300

    adicionado separacao em botoes na pagina de contato

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,17 +9,17 @@ export const routes: Routes = [
   {
     path: 'projetos',
     loadComponent: () => import('./features/projects/projects').then(c => c.Projects),
-    data: { title: 'Projetos | Wilson Oliveira', description: 'Confira meus projetos Full Stack.' }
+    data: { title: 'Wilson Oliveira | Projetos', description: 'Confira meus projetos Full Stack.' }
   },
   {
     path: 'sobre',
     loadComponent: () => import('./features/sobre/sobre').then(c => c.Sobre),
-    data: { title: 'Sobre | Wilson Oliveira', description: 'Confira mais informacoes sobre mim.' }
+    data: { title: 'Wilson Oliveira | Sobre', description: 'Confira mais informacoes sobre mim.' }
   },
   {
     path: 'contato',
     loadComponent: () => import('./features/contact/contact').then(c => c.Contact),
-    data: { title: 'Contato | Wilson Oliveira', description: 'Confira mais informacoes de contato.' }
+    data: { title: 'Wilson Oliveira | Contato', description: 'Confira mais informacoes de contato.' }
   },
   { path: '**', redirectTo: '' } // Fallback para home
 ];
