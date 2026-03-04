69c29a62ed2068446fedfef9899772d38e4b7bfa
Wilson Rogerio <rajwii44@gmail.com>
2026-03-04 20:14:09 -0300

    adicionado animacoes as telas e corrigido erros nos estilos

--- src/app/core/animations/route.animations.ts
+++ src/app/core/animations/route.animations.ts
@@ -9,26 +9,20 @@ import {
 
 export const routeAnimations = trigger('routeAnimations', [
   transition('* <=> *', [
-    query(':enter, :leave', [
+
+    query(':enter', [
       style({
-        position: 'absolute',
-        width: '100%'
-      })
-    ], { optional: true }),
+        opacity: 0,
+        transform: 'translateY(20px)'
+      }),
+      animate(
+        '320ms cubic-bezier(0.4, 0.0, 0.2, 1)',
+        style({
+          opacity: 1,
+          transform: 'translateY(0)'
+        })
+      )
+    ], { optional: true })
 
-    group([
-      query(':leave', [
-        animate('250ms ease-out',
-          style({ opacity: 0, transform: 'translateY(10px)' })
-        )
-      ], { optional: true }),
-
-      query(':enter', [
-        style({ opacity: 0, transform: 'translateY(-10px)' }),
-        animate('250ms ease-out',
-          style({ opacity: 1, transform: 'translateY(0)' })
-        )
-      ], { optional: true })
-    ])
   ])
 ]);
